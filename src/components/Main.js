6e5cd541b49b849484cb09f57f6f0e79fde6c217
jmscoding100 <joshua.z.mcgee@gmail.com>
2025-12-01 12:46:15 -0600

    commit-6

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ const Main =()=>{
         <main className="main" id="main">
             <div className="container">
                 <p className="greetings text-capitalize text-center">We hope that you find what you're looking for 😄</p>
-                <img src="images/home_page.jpg"alt="placeholder img" className="img-fluid image" id="homeImg" />
+                <img src={process.env.PUBLIC_URL + "/images/home_page.jpg"} alt="placeholder img" className="img-fluid image" id="homeImg" />
                 
             </div>
         </main>
